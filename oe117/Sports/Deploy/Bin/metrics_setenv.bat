5b128773762b442b027fe23f165b0ac37c05f0c0
DustinGrau-PSC <dugrau@progress.com>
2018-04-26 14:43:17 -0400

    Adjusting logic for logging/metrics.

---
+++ oe117/Sports/Deploy/Bin/metrics_setenv.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Set the name of the ABLApp which you want to monitor.
+set ABLAPP_NAME=SportsDemo
+
+rem Set the URL of the server where we can reach the OEManager webapp.
+set INSTANCE_URI=http://localhost:8830
+
+exit /b 0
---
+++ oe117/Sports/Deploy/Bin/metrics_setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Set the name of the ABLApp which you want to monitor.
+ABLAPP_NAME=SportsDemo
+export ABLAPP_NAME
+
+# Set the URL of the server where we can reach the OEManager webapp.
+INSTANCE_URI=http://localhost:8830
+export INSTANCE_URI
