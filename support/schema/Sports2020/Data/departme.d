8a5da59d086df503dd4dbb70165d41548843ac54
DustinGrau-PSC <dugrau@progress.com>
2018-10-09 13:51:00 -0400

    Adding initial Sports2020 database

---
+++ support/schema/Sports2020/Data/departme.d
@@ -0,0 +1,20 @@
+"100" "Consulting"
+"200" "Administration"
+"300" "Marketing"
+"400" "Sales"
+"500" "Training"
+"600" "Development"
+"700" "Finance"
+"800" "Shipping"
+.
+PSC
+filename=Department
+records=0000000000008
+ldbname=sports2020
+timestamp=2018/08/29-17:24:18
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=UTF-8
+.
+0000000155
