046a6395234e24540f7857ee17ae5c70fd999836
DustinGrau-PSC <dugrau@progress.com>
2019-03-11 14:03:38 -0400

    Updating with OE11/OE12 versions of domain info

--- support/schema/WebState/_sec-authentication-domain.d
+++ support/schema/WebState/_sec-authentication-domain.d
@@ -1,17 +1,17 @@
 "" "_oeusertable" "Default user domain" "" "" "" yes "" "" "" "" "" ? 1
-"spark" "_extsso" "External Realm Domain" "odeb0::DLGpFWdldTtBnZPjYWjG9k3rBAYl7XphjyRLCqX9mG5MTkXoVJrikC5pxjzTaTHZ" "" "" yes "" "" "" "" "" ? 0
-"UNIX" "_oslocal" "Built-in domain for UNIX user accounts, reserved for database command line utilities" "odeb0::Hv9pFmIQtkFMVmWAdcpfj4L8TvX2REliMTevHAiKkqObV8Ox0A5a+qJAfnLLJxxo" "" "" yes "" "" "" "" "" ? 2
-"UNIXID" "_oslocal" "Built-in SSO domain to the UNIX process user, reserved for database command line utilities" "odeb0::fJ9BYo+9nqvDjsVmMpYRGmRV0vLO+PbqQxAezpHidyGq3ZEd4JyNeIG1yjxwWuan" "" "" yes "" "" "" "" "" ? 2
-"WINDOWS" "_oslocal" "Built-in domain for WINDOWS user accounts, reserved for database command line utilities" "odeb0::DDSeX/jEvOXYw00I59rGZbHj6flCn5OMPqo9Qaohtff5Py7PItPm9vRqgPkcsdgX" "" "" yes "" "" "" "" "" ? 2
-"WINDOWSID" "_oslocal" "Built-in SSO domain to the WINDOWS process user, reserved for database command line utilities" "odeb0::rx6JIWQLR1ZC83Aifk0J4wrcIR8PdiHKhe0n+eDeip4jdrbom9umcvOYUNJC/oxz" "" "" yes "" "" "" "" "" ? 2
+"spark" "_extsso" "Default domain for Spark project, access via Spring Framework for authentication" "23222e35397562" "" "" yes "" "" "" "" "" ? 0
+"UNIX" "_oslocal" "Built-in domain for UNIX user accounts, reserved for database command line utilities" "34303a333b297e203533236a39202a" "" "" yes "" "" "" "" "" ? 2
+"UNIXID" "_oslocal" "Built-in SSO domain to the UNIX process user, reserved for database command line utilities" "34303a333b297e203533236a39202a" "" "" yes "" "" "" "" "" ? 2
+"WINDOWS" "_oslocal" "Built-in domain for WINDOWS user accounts, reserved for database command line utilities" "34303a333b297e203533236a39202a" "" "" yes "" "" "" "" "" ? 2
+"WINDOWSID" "_oslocal" "Built-in SSO domain to the WINDOWS process user, reserved for database command line utilities" "34303a333b297e203533236a39202a" "" "" yes "" "" "" "" "" ? 2
 .
 PSC
 filename=_sec-authentication-domain
 records=0000000000006
 ldbname=webstate
-timestamp=2019/03/08-13:26:35
+timestamp=2016/01/15-14:15:18
 numformat=44,46
 dateformat=mdy-1950
 cpstream=UTF-8
 .
-0000001086
+0000000924
