7b4bc819ee5810eac506d7570aabd6d32fbfb4d1
DustinGrau-PSC <dugrau@progress.com>
2020-10-22 16:31:20 -0400

    Fixing SOAP examples

--- oe1176/Sports/Deploy/SOAP/runbproxygen.bat
+++ oe1176/Sports/Deploy/SOAP/runbproxygen.bat
@@ -1,2 +1,2 @@
 REM Command to run batch Proxy Generation
-"C:\Progress\OpenEdge\bin\bproxygen.bat" -xpxgfile="D:\OpenSource\Spark-Toolkit-Demos\oe1176\DynSports\Deploy\SOAP\WSTest.xpxg" -startdir="C:\OpenEdge\WRK"
+"C:\Progress\OpenEdge\bin\bproxygen.bat" -xpxgfile="D:\OpenSource\Spark-Toolkit-Demos\oe1176\Sports\Deploy\SOAP\WSTest.xpxg" -startdir="C:\OpenEdge\WRK"
