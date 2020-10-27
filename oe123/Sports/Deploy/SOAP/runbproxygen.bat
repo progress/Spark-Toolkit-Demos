0e0caaef17f09045f85eb2bf9ed526b5070452ee
DustinGrau-PSC <dugrau@progress.com>
2020-10-27 08:21:29 -0400

    Update demos with 12.3 versions.

---
+++ oe123/Sports/Deploy/SOAP/runbproxygen.bat
@@ -0,0 +1,2 @@
+REM Command to run batch Proxy Generation
+"C:\Progress\OpenEdge\bin\bproxygen.bat" -xpxgfile="D:\OpenSource\Spark-Toolkit-Demos\oe122\Sports\Deploy\SOAP\WSTest.xpxg" -startdir="C:\OpenEdge\WRK"
